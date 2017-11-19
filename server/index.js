7eb3b89b9c9b8b3aca4d7881f711ff742157c6d5
fvrthebrave <collin.desoto@gmail.com>
2017-11-19 19:40:32 +0000

    route changes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express'),
       app = express();
       
+const PORT = process.env.PORT || 5000;
+      
 app.get('/', (req, res) => {
     res.send({hi: 'There'}); 
 });
 
-app.listen(process.env.PORT, process.env.IP, () => {
+
+
+app.listen(PORT, process.env.IP, () => {
     console.log('Server is listening..');  
-})
+});
