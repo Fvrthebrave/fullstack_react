6b36fd2ba4750026158e79bf3d4220f359bac571
fvrthebrave <collin.desoto@gmail.com>
2017-11-22 08:12:52 +0000

    tweak strategy settings

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,7 +23,8 @@ passport.use(
     {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: 'https://learning-space-fvrthebrave.c9users.io/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
     }, 
     (accessToken, refreshToken, profile, done) => {
         User.findOne({googleId: profile.id})
