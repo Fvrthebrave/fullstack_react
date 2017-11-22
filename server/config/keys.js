17db07202204d1fd5bb0e9c3e25c5ac3fd54f6a3
fvrthebrave <collin.desoto@gmail.com>
2017-11-22 07:57:30 +0000

    unit testing

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,9 +1,11 @@
 // keys.js - figure out what set of credentials to return
 if(process.env.NODE_ENV === 'production') {
     // we are in production, return the prod set of keys
+    console.log('Production');
     module.exports = require('./prod');
 } else {
     // we are in dev, return the dev set of keys
+    console.log('Dev');
     module.exports = require('./dev');
 }
     
